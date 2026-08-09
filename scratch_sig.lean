cbe7e605a486e4438d5eab41fa2087e23f3c1db0
Phylliida <phylliidadev@gmail.com>
2026-08-09 15:28:24 -0700

    remove stray signature-probe scratch (was committed accidentally)

--- scratch_sig.lean
+++
@@ -1,6 +0,0 @@
-import LeanNonlinearArith.Nlsat.Refute
-import LeanNonlinearArith.Nlsat.MPolyFactor
-open LeanNonlinearArith.Nlsat
-#eval MPoly.factorM [(1, [(0, 3)]), ((-6), [(0, 2)]), (11, [(0, 1)]), ((-6), [])]
-#eval MPoly.factorM [(1, [(0, 2)]), (2, [(0, 1)]), (1, [])]
-#eval MPoly.factorM [(1, [(0, 3)]), (3, [(0, 2)]), (3, [(0, 1)]), (1, [])]
